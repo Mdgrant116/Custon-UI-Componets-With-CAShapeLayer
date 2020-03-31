7ebfe720e67aff89a5429d7150baa218f2146f2c
Michael Grant <michael.grand@meijer.com>
2020-03-31 15:54:32 -0400

    Shapes created

--- Custon UI Componets With CAShapeLayer/ViewController.swift
+++ Custon UI Componets With CAShapeLayer/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let squareShapeLayer: CAShapeLayer = CAShapeLayer()
+    let triangleShapeLayer: CAShapeLayer = CAShapeLayer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createSquareShape()
+        createTriangleShape()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        squareShapeLayer.path = UIBezierPath(rect: CGRect(x: 50, y: 50, width: 100, height: 100)).cgPath
+        
+        let trianglePath = UIBezierPath()
+        trianglePath.move(to: CGPoint(x: 50, y: 300))
+        trianglePath.addLine(to: CGPoint(x: 150, y: 300))
+        trianglePath.addLine(to: CGPoint(x: 100, y: 120))
+        trianglePath.close()
+        triangleShapeLayer.path = trianglePath.cgPath
     }
 
-
+    func createSquareShape() {
+        squareShapeLayer.fillColor = UIColor.red.cgColor
+        view.layer.addSublayer(squareShapeLayer)
+        
+    }
+    
+    func createTriangleShape() {
+        triangleShapeLayer.fillColor = UIColor.gray.cgColor
+        view.layer.addSublayer(triangleShapeLayer)
+    }
 }
 
